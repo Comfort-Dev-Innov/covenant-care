bc1a38649d86c986ede70801a34caa16f602c3cd
Angelou Sereño <115084999+DanRed22@users.noreply.github.com>
2025-10-27 00:09:33 +0800

    Feature/management team (#4)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 import OurStory from "@/components/sections/OurStory";
 
 import SatellitesSection from "@/components/sections/SatellitesSection";
-
-// import ManagmentTeamSection from "@/components/sections/ManagmentTeamSection";
 import CTASection from "@/components/sections/CTASection";
+import ManagmentTeamSection from "@/components/sections/ManagmentTeamSection";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-background">
       <OurStory />
+      <ManagmentTeamSection />
       <SatellitesSection />
       <CTASection />
     </div>
---
+++ src/constants/teammatesData.ts
@@ -0,0 +1,28 @@
+export interface Teammate {
+  name: string;
+  image: string;
+  position: string;
+}
+
+export const teammatesData: Teammate[] = [
+  {
+    name: "Rosalyn Pilar C. Saavedra",
+    image: "/assets/image/rosalyn-pilar.png",
+    position: "President/Chairman",
+  },
+  {
+    name: "Rizelda D. Vidal",
+    image: "/assets/image/rizelda.png",
+    position: "Finance/Treasurer",
+  },
+  {
+    name: "Alain Rodney S. Severino",
+    image: "/assets/image/alain-rodney.png",
+    position: "CEO",
+  },
+  {
+    name: "Janfilly Galumo",
+    image: "/assets/image/janfilly.png",
+    position: "Corporate Secretary",
+  },
+];
